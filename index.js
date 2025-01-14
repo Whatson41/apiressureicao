97b22b777e8cbde391ea32341fb795226cca12cc
Isabellelorrane0009 <131703294+Isabellelorrane0009@users.noreply.github.com>
2025-01-13 23:00:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const port = 3000;
 // Configuração do banco de dados PostgreSQL
 // const pool = new Pool({
 
-bd_funeraria_user
-ZZM8wFsEESt0UnNVhcSpWV6ydLVMgKr2
-dpg-cu2lparqf0us73bp3oig-a.oregon-postgres.render.com
-bd_funeraria
+//bd_funeraria_user
+//ZZM8wFsEESt0UnNVhcSpWV6ydLVMgKr2
+//dpg-cu2lparqf0us73bp3oig-a.oregon-postgres.render.com
+//bd_funeraria
 
 
 //     user: 'postgres',
@@ -45,7 +45,7 @@ app.get('/itens', async (req, res) => {
     console.log(req)
     try {
       //  const result = await pool.query('SELECT * FROM itens');
-        const result = await pool.query('SELECT * FROM tab_cliente');
+        const result = await pool.query('SELECT * FROM tab_produtos');
         res.json(result.rows);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -53,12 +53,13 @@ app.get('/itens', async (req, res) => {
 });
 
 // Rota para inserir um novo item
+//nome,descrição,quantidade e preço
 app.post('/itens', async (req, res) => {
     console.log(req)
     const { nome, descricao } = req.body;
     try {
         const result = await pool.query(
-            'INSERT INTO itens (nome, descricao) VALUES ($1, $2) RETURNING *',
+            'INSERT INTO tab_produtos (nome, descricao,quantidade,preco) VALUES ($1, $2, $3, $4) RETURNING *',
             [nome, descricao]
         );
         res.status(201).json(result.rows[0]);
@@ -71,7 +72,7 @@ app.post('/itens', async (req, res) => {
 app.delete('/itens/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const result = await pool.query('DELETE FROM itens WHERE id = $1 RETURNING *', [id]);
+        const result = await pool.query('DELETE FROM tab_produtos WHERE id = $1 RETURNING *', [id]);
         if (result.rowCount === 0) {
             return res.status(404).json({ error: 'Item não encontrado' });
         }
@@ -118,20 +119,6 @@ app.delete('/users/:id', async (req, res) => {
         res.status(500).json({ error: 'Erro ao deletar usuário' });
     }
 });
-
-
-async function inserirProduto(desc, valor) {
-    try {
-        const sql = 'INSERT INTO tab_produto (desc_produto,valor_produto) VALUES ($1, $2)'
-        const values = [desc, valor]
-        await client.query(sql, values);
-        await client.end()
-        console.log.end()
-        console.log('Dados foram inseridos')
-    } catch (err) {
-        console.error('Não foi inserido')
-    }
-}
 
 async function inserirEstoque(produto, quantidade) {
     try {
@@ -185,15 +172,15 @@ async function verificarDocumentacao(cliente, plano, corpo, carro, servicos, ser
     }
 }
 
-async function fetchProdutos() {
-    try {
-        const response = await fetch('http://localhost:3000/produtos');
-        const produtos = await response.json();
-        renderTable(produtos);
-    } catch (err) {
-        console.error('Erro ao carregar produtos:', err);
-    }
-}
+//async function fetchProdutos() {
+  //  try {
+    //    const response = await fetch('http://localhost:3000/produtos');
+        //const produtos = await response.json();
+      //  renderTable(produtos);
+    //} catch (err) {
+     //   console.error('Erro ao carregar produtos:', err);
+    //}
+//}
 
 function renderTable(produtos) {
     const tbody = document.querySelector('#tab_estoque tbody');
